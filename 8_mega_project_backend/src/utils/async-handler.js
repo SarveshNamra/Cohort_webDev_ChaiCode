485c3fcfca134c458e096d94739c1328d2312b0a
SarveshNamra <sarveshcnamra@gmail.com>
2026-07-20 19:19:36 +0530

    Went through previous code

--- 8_mega_project_backend/src/utils/async-handler.js
+++ 8_mega_project_backend/src/utils/async-handler.js
@@ -1,6 +1,6 @@
 // ---- asyncHandler is the solution repetative try-catch  ----
 
-const asyncHandler = (reguestHandler) => {
+const asyncHandler = (reguestHandler) => {  //  Here, asyncHandler is a higher-order function, meaning it takes another function as an argument.
     return function(req, res, next){     // return is high-order function
         /*  - If requestHandler is an async function, it returns a Promise.
             - If it’s not a Promise, Promise.resolve(...) will wrap it in one anyway, making this universal. */
